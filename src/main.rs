7751766da3cda7818b95a5dfdf0818ad21d2d48d
Marie-Charlotte Calisto <marie-charlotte.calisto@epita.fr>
2020-07-15 21:53:44 +0200

    [ADD][BASE] - add base : ask how many bombs and print the bomb number to play with

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+fn get_bomb_number() -> i32
+{
+    let mut bomb = String::new();
+    let mut nb_bomb : i32;
+    loop
+    {
+        println!("How many bombs do you want to play with ?");
+        if std::io::stdin().read_line(&mut bomb).is_err() {
+            println!("error: error read_line" );
+            continue;
+        }
+
+        bomb.pop();
+        nb_bomb = match bomb.parse::<i32>()
+        {
+            Ok(n) => n,
+            Err(_e) => -1,
+        };
+        if nb_bomb == -1 {
+            println!("Not a valid number.");
+            bomb = String::new();
+            continue;
+        }
+        break;
+    }
+    nb_bomb
+}
+
+
+fn main()
+{
+    let nb_bomb = get_bomb_number();
+    println!("{:?}", nb_bomb);
+}
