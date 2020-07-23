caa9a7411975709c287ecadd87161e4c83865901
Marie-Charlotte Calisto <marie-charlotte.calisto@epita.fr>
2020-07-23 23:49:27 +0200

    main : create a sepatate file for default value

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,15 @@
+mod my_game;
+mod my_event;
+mod default;
+mod tile;
+mod board;
+
 use std::path;
 
 use ggez::{ContextBuilder, GameResult};
 use ggez::event;
 
-mod my_game;
-mod my_event;
 use my_game::MyGame;
-
-const DEFAULT_BOMB_NUMBER : u32 = 100;
-const DEFAULT_BOARD_SIZE : (usize, usize) = (30, 16);
-const DEFAULT_TILE_SIZE : (usize, usize) = (32, 32);
-const DEFAULT_SCREEN_SIZE : (f32, f32) = (
-    (DEFAULT_BOARD_SIZE.0 as f32)* (DEFAULT_TILE_SIZE.0 as f32),
-    (DEFAULT_BOARD_SIZE.1 as f32)* (DEFAULT_TILE_SIZE.1 as f32)
-    );
-
 
 
 fn main() -> GameResult {
@@ -23,7 +18,7 @@ fn main() -> GameResult {
     let (ctx, events_loop) = &mut ContextBuilder::new("minesweeper", "macha")
         .add_resource_path(resource_dir)
         .window_setup(ggez::conf::WindowSetup::default().title("minesweeper").vsync(false))
-        .window_mode(ggez::conf::WindowMode::default().dimensions(DEFAULT_SCREEN_SIZE.0, DEFAULT_SCREEN_SIZE.1))
+        .window_mode(ggez::conf::WindowMode::default().dimensions(default::SCREEN_SIZE.0, default::SCREEN_SIZE.1))
         .build()?;
 
     let my_game = &mut MyGame::new(ctx)?;
