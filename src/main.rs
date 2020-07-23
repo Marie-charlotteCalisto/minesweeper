a212c6029a9545063c883e2789dbe6195f66c431
Marie-Charlotte Calisto <marie-charlotte.calisto@epita.fr>
2020-07-23 13:16:59 +0200

    main : add - add mutable ctx and event

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use ggez::{ContextBuilder, GameResult};
 use ggez::event;
 
 mod my_game;
+mod my_event;
+use my_game::MyGame;
 
 const DEFAULT_BOMB_NUMBER : u32 = 100;
 const DEFAULT_BOARD_SIZE : (usize, usize) = (30, 16);
@@ -18,15 +20,15 @@ const DEFAULT_SCREEN_SIZE : (f32, f32) = (
 fn main() -> GameResult {
     let resource_dir = path::PathBuf::from("./resources");
 
-    let (ctx, events_loop) = ContextBuilder::new("minesweeper", "macha")
+    let (ctx, events_loop) = &mut ContextBuilder::new("minesweeper", "macha")
         .add_resource_path(resource_dir)
         .window_setup(ggez::conf::WindowSetup::default().title("minesweeper").vsync(false))
         .window_mode(ggez::conf::WindowMode::default().dimensions(DEFAULT_SCREEN_SIZE.0, DEFAULT_SCREEN_SIZE.1))
         .build()?;
 
-    let mut my_game = MyGame::new(&mut ctx)?;
+    let my_game = &mut MyGame::new(ctx)?;
 
-    match event::run(&mut ctx, &mut events_loop, &mut my_game) {
+    match event::run(ctx, events_loop, my_game) {
         Ok(_) => println!("Exited cleanly."),
         Err(e) => println!("Error occured : {}", e)
     }
