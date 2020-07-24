f9dcf3f21d27c4994009763d38d6fd0425c9f579
Marie-Charlotte Calisto <marie-charlotte.calisto@epita.fr>
2020-07-24 21:41:56 +0200

    my_event - clean and add game over and game win

--- src/my_event.rs
+++ src/my_event.rs
@@ -1,11 +1,9 @@
-use ggez::{event, Context, GameResult};
-use ggez::graphics::{self, Rect};
-use ggez::nalgebra::{Point2};
+use ggez::{graphics, event, Context, GameResult};
 use ggez::event::MouseButton;
 
-use crate::my_game::{MyGame};
+use crate::my_game::MyGame;
 use crate::default;
-use crate::tile;
+use crate::board;
 
 impl event::EventHandler for MyGame {
     fn update(&mut self, _ctx : &mut Context) -> GameResult<()> {
@@ -13,46 +11,20 @@ impl event::EventHandler for MyGame {
     }
 
     fn draw(&mut self, ctx : &mut Context) -> GameResult<()> {
+
         graphics::clear(ctx, [0.0, 0.0, 0.0, 1.0].into());
-
-        for i in 0..self.board.get_width() {
-            for j in 0..self.board.get_height() {
-                // let i_float = i as f32;
-                // let j_float = j as f32;
-
-                let tile = self.board.get(j, i);
-                let p =
-                    if !tile.is_discovered(){
-                        if tile.is_flagged(){
-                            graphics::DrawParam::new()
-                                .src(Rect::new(2. / 8., 1. / 8., 1. / 8., 1. / 8.))
-                                .dest(Point2::new((i * default::TILE_SIZE.0) as f32, (j * default::TILE_SIZE.1) as f32))
-                        }else{
-                            graphics::DrawParam::new()
-                                .src(Rect::new(2. / 8., 2. / 8., 1. / 8., 1. / 8.))
-                                .dest(Point2::new((i * default::TILE_SIZE.0) as f32, (j * default::TILE_SIZE.1) as f32))
-                        }
-                    }
-
-                    else {
-                        match tile.get_tile_type() {
-                            tile::TileType::Hint(hint) => {
-                                graphics::DrawParam::new()
-                                    .src(Rect::new(((hint % 2) as f32)/ 8., ((hint / 2) as f32) / 8., 1. / 8., 1. / 8.))
-                                    .dest(Point2::new((i * default::TILE_SIZE.0) as f32, (j * default::TILE_SIZE.1) as f32))
-                            }
-                            _ => {
-                                graphics::DrawParam::new()
-                                    .src(Rect::new(2. / 8., 0. / 8., 1. / 8., 1. / 8.))
-                                    .dest(Point2::new((i * default::TILE_SIZE.0) as f32, (j * default::TILE_SIZE.1) as f32))
-                            },
-                        }
-
-                    };
-
-                self.spritebatch.add(p);
-            }
+        if self.is_replay() {
+            self.board = board::Board::new_randomized()?;
+            self.set_ongoing();
+        }else if self.is_game_over(){
+            self.display_game_over();
+        }else if self.is_game_won(){
+            self.display_game_won();
         }
+        else{
+            self.display_ongoing_game();
+        }
+
 
         graphics::draw(ctx, &self.spritebatch, graphics::DrawParam::new())?;
 
@@ -73,8 +45,19 @@ impl event::EventHandler for MyGame {
 
         match button {
             MouseButton::Right => self.board.tile_set_flagged(i, j),
-            MouseButton::Left => self.board.tile_set_discovered(i, j),
+            MouseButton::Left => {
+                if self.is_game_over() || self.is_game_won(){
+                    self.set_replay();
+                }else if self.board.get(i, j).is_bomb(){
+                    self.set_game_over();
+                }
+                self.board.tile_set_discovered(i, j);
+            },
             _ => print!("Not handled"),
         };
+
+        if !self.is_replay() && self.board.is_completed(){
+            self.set_game_won();
+        }
     }
 }
